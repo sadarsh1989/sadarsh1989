5f547811440efdf54f00b9530e953a7d5ab2ee79
sadarsh1989 <71517075+sadarsh1989@users.noreply.github.com>
2023-04-28 18:46:04 -0400

    Update delete_tilde.js

--- delete_tilde.js
+++ delete_tilde.js
@@ -3,15 +3,17 @@ const csv = require('csv-parser');
 
 const filePath = '/path/to/csv/file.csv'; // Replace with your CSV file path
 const delimiter = ','; // Replace with your CSV delimiter
+const columnName = 'columnName'; // Replace with the name of the column to modify
 
 fs.createReadStream(filePath)
   .pipe(csv({ separator: delimiter }))
   .on('data', row => {
-    const segments = row['columnName'].split(delimiter); // Replace with the name of the column that contains the segments
-    const newSegments = segments.slice(0, 13); // Keep the first 13 segments and discard the rest
-    const newRow = newSegments.join(delimiter); // Join the remaining segments with the CSV delimiter
-    // Do something with the new row (e.g. write it to a new CSV file)
-    console.log(newRow);
+    const value = row[columnName];
+    const index = value.indexOf(delimiter);
+    const newValue = index >= 0 ? value.substring(0, index) : value; // Keep everything before the delimiter, or the entire string if the delimiter is not found
+    row[columnName] = newValue; // Update the value of the column
+    // Do something with the modified row (e.g. write it to a new CSV file)
+    console.log(row);
   })
   .on('end', () => {
     console.log('CSV file successfully processed.');
